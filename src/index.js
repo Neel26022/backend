068abe9f545951c484a66a05b21575aebf3744f4
Neel Prajapati <neelprajapati260@gmail.com>
2024-05-13 03:40:28 -0700

    utils code dissussion

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at prot : ${process.env.PORT}`)
+        app.on("error", (err) => {
+            console.error("Server running error: ",err)
+            throw err
+        })
+    })
+)
+.catch((err)=>{
+    console.log("Mongodb connection failed !!!",err);
+})
---
+++ src/utils/apiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error{
+    constructor(
+        statuseCode,
+        message= "Something went wrong",
+        errors= [],
+        statck = ""
+    ){
+        super(message)
+        this.statuseCode = statuseCode
+        this.data = null 
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+
+        if(statck) {
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
