80e246f35f787cee30a14a4c7d5ff46940d0be66
Neel Prajapati <neelprajapati260@gmail.com>
2024-05-13 19:54:51 -0700

    add cloudinary file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+import {v2 as cloudinary} from 'cloudinary';
+          
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+  api_key: process.env.CLOUDINARY_API_KEY , 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            console.error("Not fide loaclpath")
+            throw error
+        } 
+        //upload file on cludinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded successfully
+        console.log("file is uploaded at cloudinary",response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally saved file as the upload operation got faild
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
+
