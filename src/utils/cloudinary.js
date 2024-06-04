a2c1247209f0b1b217b2246a1510efc208c3be7a
Neel Prajapati <neelprajapati260@gmail.com>
2024-06-04 05:16:48 -0700

    add fuction deleteOnCloudinary and bug fix

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,32 +1,42 @@
-import { v2 as cloudinary } from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
-          
+
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
-  api_key: process.env.CLOUDINARY_API_KEY , 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) {
-            console.error("Not finde loaclpath")
-            throw error
-        } 
-        //upload file on cludinary
+        if (!localFilePath) return null
+        //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        //file has been uploaded successfully
-        console.log("file is uploaded at cloudinary",response.url)
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
-        return response
+        return response;
+
     } catch (error) {
-        fs.unlinkSync(localFilePath)//remove the locally saved file as the upload operation got faild
-        return null
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
     }
 }
 
-export {uploadOnCloudinary}
+const deleteOnCloudinary = async (oldImageCloudinaryPath) => {
+    try {
+        cloudinary.uploader.destroy(oldImageCloudinaryPath)
+    } catch (error) {
+        console.log("erro is:", error)
+    }
+}
 
+
+
+export {
+    uploadOnCloudinary,
+    deleteOnCloudinary
+}
